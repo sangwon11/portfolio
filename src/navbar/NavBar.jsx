8958f2967241ecafbeb6c905abbfb19610ab3cab
sangwon11 <dbstkd0408@gmail.com>
2024-09-16 22:51:14 +0900

    nav tailwind적용

--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import './NavBar.css';
 import { Outlet } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div className='layout'>
-    <div className='nav'>
-      <div className='nav-title'>PORTFOLIO</div>
-      <div className='nav-link'>
+    <div className='w-screen'>
+    <div className='w-screen h-[100px] fixed flex justify-between items-center border-solid border-b-2'>
+      <div className='text-[40px] font-bold pl-[30px]'>PORTFOLIO</div>
+      <div className='flex justify-between w-[500px] text-[30px] font-bold pr-[30px]'>
         <div>About Me</div>
         <div>Git Hub</div>
         <div>Skill</div>
