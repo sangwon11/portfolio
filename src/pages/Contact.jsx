7ab5b59197aa7bddec7fbd791812c804e493bc64
sangwon11 <dbstkd0408@gmail.com>
2024-09-19 02:38:49 +0900

    project

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='flex flex-col justify-center items-center w-[100vw] h-[100vh]'>contact</div>
+    <div className='flex flex-col justify-center items-center w-[100vw] h-[100vh]'></div>
   )
 }
 
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Project = () => {
   return (
-    <div className='flex flex-col justify-center items-center w-[100vw] h-[100vh]'>Project</div>
+    <div className='flex flex-col justify-center items-center w-[100vw] h-[100vh]'>Project
+      <div className='w-[1100px] h-[800px] flex flex-wrap justify-between items-center'>
+        <div className='w-[500px] h-[300px] border-2'></div>
+        <div className='w-[500px] h-[300px] border-2'></div>
+        <div className='w-[500px] h-[300px] border-2'></div>
+        <div className='w-[500px] h-[300px] border-2'></div>
+      </div>
+    </div>
   )
 }
 
