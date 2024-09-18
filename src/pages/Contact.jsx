ed2062808fa929ae90fd0fc66f01a3ab49dc7f04
sangwon11 <dbstkd0408@gmail.com>
2024-09-19 02:43:15 +0900

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='flex flex-col justify-center items-center w-[100vw] h-[100vh]'></div>
+    <div className='flex flex-col justify-center items-center w-[100vw] h-[100vh]'>
+      <a href='https://github.com/'>깃허브</a>
+    </div>
   )
 }
 
