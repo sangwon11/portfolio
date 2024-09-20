6ca9bc4ad4a4c6da8885704188efb71b4e7e2003
sangwon11 <dbstkd0408@gmail.com>
2024-09-21 00:31:54 +0900

    project links

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -2,12 +2,24 @@ import React from 'react'
 
 const Project = () => {
   return (
-    <div className='flex flex-col justify-center items-center w-[100vw] h-[100vh]'>Project
-      <div className='w-[1100px] h-[800px] flex flex-wrap justify-between items-center'>
-        <div className='w-[500px] h-[300px] border-2'></div>
-        <div className='w-[500px] h-[300px] border-2'></div>
-        <div className='w-[500px] h-[300px] border-2'></div>
-        <div className='w-[500px] h-[300px] border-2'></div>
+    <div className='flex flex-col justify-center items-center w-[100vw] h-[100vh]'>
+      <div className='w-[1100px] h-[800px] flex flex-wrap justify-between items-center text-[25px] font-extrabold'>
+        <div className='flex flex-col items-center justify-between h-[350px]'>
+          <a href='https://weather-vibe.netlify.app/' className='w-[500px] h-[300px] border-2'></a>
+          <div>Weather-Vibe</div>
+        </div>
+        <div className='flex flex-col items-center justify-between h-[350px]'>
+          <a href='https://fluegel.netlify.app/' className='w-[500px] h-[300px] border-2'></a>
+          <div>Flugel</div>
+        </div>
+        <div className='flex flex-col items-center justify-between h-[350px]'>
+        <a href='https://paradox1.netlify.app/' className='w-[500px] h-[300px] border-2'></a>
+        <div>Paradox</div>
+        </div>
+        <div className='flex flex-col items-center justify-between h-[350px]'>
+        <a href='https://withbuddy.netlify.app/' className='w-[500px] h-[300px] border-2'></a>
+        <div>Buddy</div>
+        </div>
       </div>
     </div>
   )
