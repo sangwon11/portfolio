924fa51efd6722a247482cb32b6af317c591167c
sangwon11 <dbstkd0408@gmail.com>
2024-09-21 00:17:06 +0900

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import githubIcon from '../assets/icons/githubIcon.svg'
+import emailIcon from '../assets/icons/emailIcon.svg'
 
 const Contact = () => {
   return (
-    <div className='flex flex-col justify-center items-center w-[100vw] h-[100vh]'>
-      <a href='https://github.com/sangwon11'>깃허브</a>
+    <div className='flex flex-col items-center justify-around w-[100vw] h-[100vh] text-[25px] font-extrabold'>
+      <div className='flex flex-col items-center justify-center'>
+        <img src={emailIcon} width={100}></img>
+        <div>Mail</div>
+        <div>dbstkd@gmail.com</div>
+      </div>
+      <div className='flex flex-col items-center'>
+        <a className='flex flex-col items-center' href='https://github.com/sangwon11'>
+          <img src={githubIcon} width={100}></img>
+          <div>GitHub</div>
+        </a>
+      </div>
     </div>
   )
 }
