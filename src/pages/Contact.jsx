5224c0a06e58ec870ca04aaec1380c474499fbad
sangwon11 <dbstkd0408@gmail.com>
2024-09-20 20:23:06 +0900

    github address

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className='flex flex-col justify-center items-center w-[100vw] h-[100vh]'>
-      <a href='https://github.com/'>깃허브</a>
+      <a href='https://github.com/sangwon11'>깃허브</a>
     </div>
   )
 }
