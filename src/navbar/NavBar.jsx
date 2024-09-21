67dc9c9d8101fe3cbf8b64c9e39d4a8098d6a55f
sangwon11 <dbstkd0408@gmail.com>
2024-09-22 00:31:02 +0900

    nav fff

--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-scroll';
 const NavBar = () => {
   return (
     <div className='w-screen'>
-    <div className='w-screen h-[100px] fixed flex sm:flex-row flex-col justify-between items-center border-solid border-b-2'>
+    <div className='w-screen bg-[#fff] z-10 h-[100px] fixed flex sm:flex-row flex-col justify-between items-center border-solid border-b-2'>
       <div className='text-[40px] font-bold sm:pl-[30px] pl-[0]'>PORTFOLIO</div>
       <div className='flex sm:justify-between justify-around sm:w-[500px] w-[300px] sm:text-[30px] text-[15px] font-bold sm:pr-[30px] pr-[0]'>
           <Link to="about-me" smooth={true} duration={500} className="cursor-pointer z-10">About Me</Link>
