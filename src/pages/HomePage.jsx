3e6521de65350eb622e094014d00fb8ab47583bb
sangwon11 <dbstkd0408@gmail.com>
2024-09-21 23:02:06 +0900

    aboutme 반응형

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -18,49 +18,49 @@ const HomePage = () => {
 
   return (
     <div>
-      <main className="pt-10">
-        <div className="flex justify-center items-center w-[100vw] h-[100vh]">
+      <main className="pt-20">
+        <div className="flex flex-wrap justify-center items-center w-[100vw] h-[100vh]">
           <div>
-            <img className="w-[500px] h-[610px] border-1" />
+            <img className="sm:w-[500px] w-[300px] sm:h-[610px] h-[300px] border-1" />
           </div>
-          <div ref={aboutmeRef} style={aboutmeStyle} className="w-[500px] h-[610px] pt-[50px] pl-[40px]">
-            <div className="text-[30px] font-extrabold">
+          <div ref={aboutmeRef} style={aboutmeStyle} className="sm:w-[500px] w-[300px] sm:h-[610px] h-[380px] pt-[50px] pl-[40px]">
+            <div className="sm:text-[30px] text-[15px] font-extrabold">
               <div>안녕하세요.</div>
               <div>프론트엔드 개발자 윤상원입니다.</div>
             </div>
-            <div className="text-[30px] font-extrabold">
+            <div className="sm:text-[30px] text-[15px] font-extrabold">
               Skills
             </div>
-            <div className="text-[30px] font-extrabold">
+            <div className="sm:text-[30px] text-[15px] font-extrabold">
               Laguage/Library/Flamework
             </div>
-            <div className='flex flex-wrap justify-between w-[480px] h-[300px]'>
+            <div className='flex flex-wrap justify-between sm:w-[480px] w-[240px] sm:h-[300px] h-[150px]'>
               <div className='flex flex-col items-center justify-center'>
-                <img src={htmlIcon} alt="html icon" width={100} />
+                <img src={htmlIcon} alt="html icon" className='sm:w-[100px] w-[50px]' />
                 <div>HTML</div>
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <img src={cssIcon} alt="css icon" width={100} />
+                <img src={cssIcon} alt="css icon"  className='sm:w-[100px] w-[50px]' />
                 <div>CSS</div>
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <img src={jsIcon} alt="js icon" width={100} />
+                <img src={jsIcon} alt="js icon"  className='sm:w-[100px] w-[50px]' />
                 <div>Javascript</div>
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <img src={reactIcon} alt="react icon" width={100} />
+                <img src={reactIcon} alt="react icon"  className='sm:w-[100px] w-[50px]' />
                 <div>React.js</div>
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <img src={reduxIcon} alt="redux icon" width={100} />
+                <img src={reduxIcon} alt="redux icon"  className='sm:w-[100px] w-[50px]' />
                 <div>Redux</div>
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <img src={reactqueryIcon} alt="reactquery icon" width={100} height={100} />
+                <img src={reactqueryIcon} alt="reactquery icon"  className='sm:w-[100px] w-[50px] sm:h-[100px] h-[50px]' />
                 <div>React-Query</div>
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <img src={tailwindIcon} alt="tailwind icon" width={100} />
+                <img src={tailwindIcon} alt="tailwind icon"  className='sm:w-[100px] w-[50px]' />
                 <div>Tailwind CSS</div>
               </div>
               <div className='flex flex-col items-center justify-center'>
