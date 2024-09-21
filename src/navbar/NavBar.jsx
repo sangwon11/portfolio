a8303ea07f2984b5ebfd49700d5c7b9ddeb37e26
sangwon11 <dbstkd0408@gmail.com>
2024-09-22 03:29:08 +0900

    scroll speed

--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -8,9 +8,9 @@ const NavBar = () => {
     <div className='w-screen bg-[#fff] z-10 h-[100px] fixed flex sm:flex-row flex-col justify-between items-center border-solid border-b-2'>
       <div className='text-[40px] font-bold sm:pl-[30px] pl-[0]'>PORTFOLIO</div>
       <div className='flex sm:justify-between justify-around sm:w-[500px] w-[300px] sm:text-[30px] text-[15px] font-bold sm:pr-[30px] pr-[0]'>
-          <Link to="about-me" smooth={true} duration={500} className="cursor-pointer z-10">About Me</Link>
-          <Link to="project" smooth={true} duration={500} className="cursor-pointer z-10">Project</Link>
-          <Link to="contact" smooth={true} duration={500} className="cursor-pointer z-10">Contact</Link>
+          <Link to="about-me" smooth={true} duration={200} className="cursor-pointer z-10">About Me</Link>
+          <Link to="project" smooth={true} duration={200} className="cursor-pointer z-10">Project</Link>
+          <Link to="contact" smooth={true} duration={200} className="cursor-pointer z-10">Contact</Link>
       </div>
     </div>
     <Outlet/>
