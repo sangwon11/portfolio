8af777ff70dac6c026f6641db3742daae865067a
sangwon11 <dbstkd0408@gmail.com>
2024-09-27 03:00:14 +0900

    border none

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -7,22 +7,22 @@ const Project = () => {
         Projects
       </div>
       <div className=' sm:w-[1100px] w-[350px] h-full flex flex-col items-center justify-around sm:text-[25px] text-[10px] font-extrabold border-2 border-violet-900'>
-        <div className='group flex flex-col items-center justify-center sm:w-[1000px] w-[300px] sm:h-[500px] h-[150px] bg-weatherVibe-img bg-cover shadow-2xl border-2'>
+        <div className='group flex flex-col items-center justify-center sm:w-[1000px] w-[300px] sm:h-[500px] h-[150px] bg-weatherVibe-img bg-cover shadow-2xl'>
           <a href='https://weather-vibe.netlify.app/' target='_blank' className='opacity-0 group-hover:opacity-100 flex justify-center items-center sm:w-[500px] w-[100px] sm:h-[100px] h-[20px] bg-weatherVibe-color rounded-xl text-[#fff] text-center border-2 transition-opacity duration-500'>
             Weather Vibe
           </a>
         </div>
-        <div className='group flex flex-col items-center justify-center sm:w-[1000px] w-[300px] sm:h-[500px] h-[150px] bg-flugel-img bg-cover shadow-2xl border-2'>
+        <div className='group flex flex-col items-center justify-center sm:w-[1000px] w-[300px] sm:h-[500px] h-[150px] bg-flugel-img bg-cover shadow-2xl'>
           <a href='https://fluegel.netlify.app/' target='_blank' className='opacity-0 group-hover:opacity-100 flex justify-center items-center sm:w-[500px] w-[100px] sm:h-[100px] h-[20px] bg-[#25252b] rounded-xl text-[#d6ed0e] border-2 transition-opacity duration-500'>
             Flugel
           </a>
         </div>
-        <div className='group flex flex-col items-center justify-center sm:w-[1000px] w-[300px] sm:h-[500px] h-[150px] bg-paradox-img bg-cover shadow-2xl border-2'>
+        <div className='group flex flex-col items-center justify-center sm:w-[1000px] w-[300px] sm:h-[500px] h-[150px] bg-paradox-img bg-cover shadow-2xl'>
         <a href='https://paradox1.netlify.app/' target='_blank' className='opacity-0 group-hover:opacity-100 flex justify-center items-center sm:w-[500px] w-[100px] sm:h-[100px] h-[20px] bg-[#000000] rounded-xl text-[#ed0000] border-2 transition-opacity duration-500'>
           Paradox
         </a>
         </div>
-        <div className='group flex flex-col items-center justify-center sm:w-[1000px] w-[300px] sm:h-[500px] h-[150px] bg-buddy-img bg-cover shadow-2xl border-2'>
+        <div className='group flex flex-col items-center justify-center sm:w-[1000px] w-[300px] sm:h-[500px] h-[150px] bg-buddy-img bg-cover shadow-2xl'>
         <a href='https://withbuddy.netlify.app/' target='_blank' className='opacity-0 group-hover:opacity-100 flex justify-center items-center sm:w-[500px] w-[100px] sm:h-[100px] h-[20px] bg-[#FF7336] rounded-xl text-[#fff] border-2 transition-opacity duration-500'>
           Buddy  
         </a>
