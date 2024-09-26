553ed4ef3b4f72e10eecbea49995a73848a00689
sangwon11 <dbstkd0408@gmail.com>
2024-09-26 23:37:17 +0900

    portfolio ui,hover

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -2,25 +2,28 @@ import React from 'react'
 
 const Project = () => {
   return (
-    <div className='flex flex-row justify-center items-start w-[100vw] h-[400vh] border-2 border-amber-800'>
-      <div className='pt-[100px] sm:w-[1100px] w-[350px] h-full flex flex-col text-[25px] font-extrabold border-2 border-violet-900'>
-        <div className='flex flex-col items-center justify-between'>
-          <a href='https://weather-vibe.netlify.app/' target='_blank' className='flex justify-center items-center bg-weatherVibe-color rounded-xl text-[#fff] text-center w-[1000px] h-[700px] border-2'>
+    <div className='flex flex-col justify-start items-center w-[100vw] h-[400vh] border-2 border-amber-800'>
+      <div className='mt-[50px] w-96 h-36 text-center text-[40px] font-extrabold flex justify-center items-center'>
+        Projects
+      </div>
+      <div className=' sm:w-[1100px] w-[350px] h-full flex flex-col items-center justify-around text-[25px] font-extrabold border-2 border-violet-900'>
+        <div className='group flex flex-col items-center justify-center w-[1000px] h-[500px] bg-weatherVibe-img bg-cover shadow-2xl border-2'>
+          <a href='https://weather-vibe.netlify.app/' target='_blank' className='opacity-0 group-hover:opacity-100 flex justify-center items-center w-[500px] h-[100px] bg-weatherVibe-color rounded-xl text-[#fff] text-center border-2 transition-opacity duration-500'>
             Weather Vibe
           </a>
         </div>
-        <div className='flex flex-col items-center justify-between'>
-          <a href='https://fluegel.netlify.app/' target='_blank' className='flex justify-center items-center bg-[#25252b] rounded-xl text-[#d6ed0e] w-[1000px] h-[700px] border-2'>
+        <div className='group flex flex-col items-center justify-center w-[1000px] h-[500px] bg-flugel-img bg-cover shadow-2xl border-2'>
+          <a href='https://fluegel.netlify.app/' target='_blank' className='opacity-0 group-hover:opacity-100 flex justify-center items-center w-[500px] h-[100px] bg-[#25252b] rounded-xl text-[#d6ed0e] border-2 transition-opacity duration-500'>
             Flugel
           </a>
         </div>
-        <div className='flex flex-col items-center justify-between'>
-        <a href='https://paradox1.netlify.app/' target='_blank' className='flex justify-center items-center bg-[#000000] rounded-xl text-[#ed0000] w-[1000px] h-[700px] border-2'>
+        <div className='group flex flex-col items-center justify-center w-[1000px] h-[500px] bg-paradox-img bg-cover shadow-2xl border-2'>
+        <a href='https://paradox1.netlify.app/' target='_blank' className='opacity-0 group-hover:opacity-100 flex justify-center items-center w-[500px] h-[100px] bg-[#000000] rounded-xl text-[#ed0000] border-2 transition-opacity duration-500'>
           Paradox
         </a>
         </div>
-        <div className='flex flex-col items-center justify-between'>
-        <a href='https://withbuddy.netlify.app/' target='_blank' className='flex justify-center items-center bg-[#FF7336] rounded-xl text-[#fff] w-[1000px] h-[700px] border-2'>
+        <div className='group flex flex-col items-center justify-center w-[1000px] h-[500px] bg-buddy-img bg-cover shadow-2xl border-2'>
+        <a href='https://withbuddy.netlify.app/' target='_blank' className='opacity-0 group-hover:opacity-100 flex justify-center items-center w-[500px] h-[100px] bg-[#FF7336] rounded-xl text-[#fff] border-2 transition-opacity duration-500'>
           Buddy  
         </a>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
     extend: {
       backgroundImage: {
         'weatherVibe-color': 'linear-gradient(45deg, #04f1dd, #057cfb)',
+        'weatherVibe-img': 'url("./assets/images/weatherVibe.png")',
+        'flugel-img': 'url("./assets/images/flugel.png")',
+        'paradox-img': 'url("./assets/images/paradox.png")',
+        'buddy-img': 'url(./assets/images/buddy.png)',
       }
     },
   },
